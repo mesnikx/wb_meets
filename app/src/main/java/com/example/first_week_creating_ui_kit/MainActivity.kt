0ea71f29fba5a62cb59aed4af5183acbd13daca1
Michael Rudakov <mishelrudakov@yandex.ru>
2024-07-13 13:07:04 +0300

    Deleting imports, and that's all

--- app/src/main/java/com/example/first_week_creating_ui_kit/MainActivity.kt
+++ app/src/main/java/com/example/first_week_creating_ui_kit/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.example.first_week_creating_ui_kit
 
-import ShowNumberPicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.first_week_creating_ui_kit.navigation.RootScreen
-import com.example.first_week_creating_ui_kit.ui.components.screens.autorization.base.PasswordPreview
 import com.example.first_week_creating_ui_kit.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
